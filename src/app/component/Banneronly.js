7017d88b326db2ab68704c94f1a2509d499bc91b
PremiumTrust Bank Digital <sobayoafolabi@yahoo.com>
2024-11-29 14:33:11 +0100

    nnnn

--- src/app/component/Banneronly.js
+++ src/app/component/Banneronly.js
@@ -56,7 +56,7 @@ export default function Banneronly() {
               <section>
                 <Image
                   className="w-100 "
-                  src={bannerslide1}
+                  src={bannerslide4}
                   alt="bannerslides"
                 />
               </section>
